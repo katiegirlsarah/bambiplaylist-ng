945a0f22b9c0fe031eb8aa66e58aaeccdc8b8728
Katie <katiegirlsarah@gmail.com>
2022-12-25 16:26:22 +1100

    (feat) add navbar and css

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,42 @@
+<header>
+  <a href="/">bambi playlist</a>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="/playlist">playlists</a>
+      </li>
+      <li>
+        <a href="/post">post</a>
+      </li>
+      <li>
+        <a href="/login">login</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    padding: 1rem;
+    background: #423;
+    color: #eee;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: -8px;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+    gap: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+  import '$lib/styles/style.css';
+</script>
+
+<Navbar />
+
+<main>
+  <slot />
+</main>
